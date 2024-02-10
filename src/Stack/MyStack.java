85f969b4c3adb838b061c5e54827d28d9fe548f5
JosephEmeka <josephemekaonah@gmail.com>
2024-02-10 17:19:52 +0100

    1.0.02

--- src/Stack/MyStack.java
+++ src/Stack/MyStack.java
@@ -1,33 +1,47 @@
 package Stack;
 
+import java.util.EmptyStackException;
+
 public class MyStack {
 
     private boolean isEmpty;
     private  String [] stackOfStrings;
-    private int size;
+    private int numberOfElements;
 
     private int capacity;
 
 
     public MyStack(int capacity){
         isEmpty = true;
+        numberOfElements = 0;
         stackOfStrings = new String[capacity];
 
     }
     public boolean stackIsEmpty() {
-        return isEmpty;
+        if (numberOfElements == 0) {
+            return isEmpty = true;
+        }
+        else {
+            return isEmpty = false;
+        }
     }
 
-    public void getSizeOfStack(int [] stackArrays){
+
+    public String pop() {
+        if (numberOfElements == 0) {
+            throw new EmptyStackException();
+        }
+        else {
+            return stackOfStrings[--numberOfElements];
+
+        }
     }
 
-    public boolean pop(String shoeName) {
-        return isEmpty = false;
+    public void push(String adidas) {
+        numberOfElements++;
     }
 
-
-    public void pop() {
-        getSizeOfStack()
-        if
+    public int size() {
+        return numberOfElements;
     }
 }
--- test/MyStackTest.java
+++ test/MyStackTest.java
@@ -1,87 +1,91 @@
 import Stack.MyStack;
 import org.junit.jupiter.api.*;
 
+import java.util.EmptyStackException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyStackTest {
     private MyStack stringStack;
 
     @BeforeEach
-    public void doThisBeforeAll(){
-         stringStack = new MyStack(4);
+    public void doThisBeforeAll() {
+        stringStack = new MyStack(5);
     }
 
     @Test
-    public void testThatStackIsEmpty(){
+    public void testThatStackIsEmpty() {
         assertTrue(stringStack.stackIsEmpty());
     }
 
     @Test
-    public void testThatWhenEmptyStackIsPopped_ThrowsOutOfBoundException(){
+    public void testThatWhenEmptyStackIsPopped_ThrowsEmptyStackException() {
         assertTrue(stringStack.stackIsEmpty());
-        stringStack.pop();
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> stringStack.pop());
-
-
+        assertThrows(EmptyStackException.class, () -> stringStack.pop());
     }
+
     @Test
-    public void testThatWhenAnElementIsAddedInStack_StackIsNotEmpty(){
-        //MyStack stackOfString = new MyStack();
-        String shoeName = "Adidas";
-        stringStack.pop(shoeName);
+    public void testThatWhenAnElementIsAddedInStack_StackIsNotEmpty() {
+        stringStack.push("Adidas");
         assertFalse(stringStack.stackIsEmpty());
     }
 
     @Test
-    public void testThatMoreThanOneElementCanBePushedIntoStack_StackNotEmpty(){
-        String shoeName = "Adidas";
-        String secondShoeName = "Nike";
-        stringStack.pop(shoeName);
-
+    public void testThatMoreThanOneElementCanBePushedIntoStack_StackNotEmpty() {
+        stringStack.push("Adidas");
+        stringStack.push("Nike");
+        assertFalse(stringStack.stackIsEmpty());
     }
 
 
     @Test
-    public void testThatElementCanBePoppedOutOfStack_StackNotEmpty(){
-        String shoeName = "Adidas";
-        String secondShoeName = "Nike";
-        stringStack.pop(shoeName);
-        stringStack.pop();
-
+    public void testThatSameElementCanBePushedIntoStack_StackNotEmpty() {
+           stringStack.push("Adidas");
+           stringStack.push("Nike");
+           stringStack.push("Adidas");
+           stringStack.push("Crocs");
+           assertEquals(4,stringStack.size());
+           stringStack.pop();
+           assertEquals(3,stringStack.size());
     }
 
     @Test
 
-    public void testThatElementPushedIntoStackIsTheLastElementInStack(){
+    public void testThatElementPushedIntoStackIsTheLastElementInStack() {
+        stringStack.push("Adidas");
+        stringStack.push("Nike");
+        assertEquals("Nike",  stringStack.pop());
 
-
-
-    }
-    @Test
-    public void testThatElementsPlacedBeforeTheLastElementInStackCannotPopped(){
-        String shoeName = "Adidas";
-        String secondShoeName = "Nike";
-        stringStack.pop(shoeName);
-        stringStack.pop();
 
     }
 
     @Test
-    public void testThatElementsCannotBePlacedPushedIntoPositionsBeforeTheLastElementInStack(){
-        String shoeName = "Adidas";
-        String secondShoeName = "Nike";
-        stringStack.pop(shoeName);
-        stringStack.pop();
+    public void testThatElementsPlacedBeforeTheLastElementInStackCannotPopped() {
+//        String shoeName = "Adidas";
+//        String secondShoeName = "Nike";
+//        stringStack.pop();
+
 
     }
 
-   @Test
-    public void testThatLastNameInStackCanBePeeked(){
-        String shoeName = "Adidas";
-        String secondShoeName = "Nike";
-        stringStack.pop(shoeName);
-        stringStack.pop();
+    @Test
+    public void testThatElementsCannotBePlacedPushedIntoPositionsBeforeTheLastElementInStack() {
+//        String shoeName = "Adidas";
+//        String secondShoeName = "Nike";
+//        stringStack.pop();
+
+
     }
+
+    @Test
+    public void testThatLastNameInStackCanBePeeked() {
+//        String shoeName = "Adidas";
+//        String secondShoeName = "Nike";
+//        stringStack.pop();
+//    }
+
+    }
+
 }
 
 
